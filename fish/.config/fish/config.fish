67fcf536a7acacb42da4208f04d15a17fddcd489
Fabio Rodrigues <fabio.rod@outlook.pt>
2026-05-03 22:36:46 +0100

    add tema ao fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,11 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
 end
-/Users/fabio/.local/bin/mise activate fish | source
 fish_add_path ~/.local/bin
-fish_add_path /Library/TeX/texbin
-fish_add_path /Applications/Ghostty.app/Contents/MacOS
+if command -q mise
+    mise activate fish | source
+end
+if test (uname) = Darwin
+    fish_add_path /Library/TeX/texbin
+    fish_add_path /Applications/Ghostty.app/Contents/MacOS
+end
