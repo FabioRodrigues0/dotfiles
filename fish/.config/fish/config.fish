bc01055f4843d30a812e584784262939059376d5
Fabio Rodrigues <fabio.rod@outlook.pt>
2026-05-10 10:48:37 +0100

    refractor: update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 end
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
@@ -10,3 +10,4 @@ if test (uname) = Darwin
     fish_add_path /Library/TeX/texbin
     fish_add_path /Applications/Ghostty.app/Contents/MacOS
 end
+alias emacs='emacs-pgtk --init-directory ~/.config/emacs'
